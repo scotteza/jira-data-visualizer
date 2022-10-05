d56b0974f3d1ad165c925737be56f4d7ff31dde6
Scott Edwards <scotteza@gmail.com>
2022-10-05 20:34:29 +0100

    Add playground test

--- JiraDataVisualizer.Test/VisualizerShould.cs
+++ JiraDataVisualizer.Test/VisualizerShould.cs
@@ -1,15 +1,13 @@
 ﻿using NUnit.Framework;
 
-namespace JiraDataVisualizer.Test
-{
-    internal class VisualizerShould
-    {
-        [Test]
-        public void Visualize_A_Thing()
-        {
-            var result = new Visualizer().Visualize();
-            Assert.True(result);
-        }
-    }
+namespace JiraDataVisualizer.Test;
 
+internal class VisualizerShould
+{
+    [Test]
+    public void Visualize_A_Thing()
+    {
+        var result = new Visualizer().Visualize();
+        Assert.True(result);
+    }
 }
