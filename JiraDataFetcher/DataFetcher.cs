c70ae0d285563dfdf97d6db9352836f716432922
Scott Edwards <scotteza@gmail.com>
2022-10-05 22:09:51 +0100

    Add basic Jira issue fetching

---
+++ JiraDataFetcher/DataFetcher.cs
@@ -0,0 +1,33 @@
+﻿using HttpWrapper;
+using System.Text.Json;
+
+namespace JiraDataFetcher;
+
+public class DataFetcher
+{
+    private readonly IHttpGetter _httpGetter;
+    private readonly string _jiraDomain;
+    private readonly string _username;
+    private readonly string _password;
+
+    public DataFetcher(IHttpGetter httpGetter, string jiraDomain, string username, string password)
+    {
+        _httpGetter = httpGetter;
+        _jiraDomain = jiraDomain;
+        _username = username;
+        _password = password;
+    }
+
+    public async Task<JiraIssue?> FetchIssue(string key)
+    {
+        var response = await _httpGetter.GetWithBasicAuthentication(
+            $"https://{_jiraDomain}.atlassian.net",
+            $"/rest/api/3/issue/{key}",
+            _username,
+            _password);
+
+        var jiraIssue = JsonSerializer.Deserialize<JiraIssue>(response);
+
+        return jiraIssue;
+    }
+}
---
+++ JiraDataFetcher/JiraIssue.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JiraDataFetcher;
+
+public class JiraIssue
+{
+    [JsonPropertyName("key")]
+    public string Key { get; }
+
+    public JiraIssue(string key)
+    {
+        Key = key;
+    }
+}
