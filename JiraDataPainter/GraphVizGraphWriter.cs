e739c2fb0865e6142fa8083480f9d46b01892c8e
Scott Edwards <scotteza@gmail.com>
2022-10-18 20:52:23 +0100

    Start batch file when GraphViz generation is done

--- JiraDataPainter/GraphVizGraphWriter.cs
+++ JiraDataPainter/GraphVizGraphWriter.cs
@@ -11,6 +11,7 @@ public class GraphVizGraphWriter : IGraphVizGraphWriter
         File.WriteAllText(fullBatchPath, @"dot -Tpng -Gdpi=150 input.dot > output.png
 code output.png");
 
-        System.Diagnostics.Process.Start("explorer.exe", $"/select, \"{fullBatchPath}\"");
+        //System.Diagnostics.Process.Start("explorer.exe", $"/select, \"{fullBatchPath}\"");
+        System.Diagnostics.Process.Start("cmd.exe", $"/c {fullBatchPath}");
     }
 }
