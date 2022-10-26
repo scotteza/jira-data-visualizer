a0fe98d2284b1219a8e58ba9cff8e624c512aa16
Scott Edwards <scott.edwards@cazoo.co.uk>
2022-10-26 10:22:36 +0100

    Add ability to generate batch/shell scripts in both MacOS and Windows

--- JiraDataPainter/GraphVizGraphWriter.cs
+++ JiraDataPainter/GraphVizGraphWriter.cs
@@ -1,18 +1,33 @@
-﻿namespace JiraDataPainter;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace JiraDataPainter;
 
 public class GraphVizGraphWriter : IGraphVizGraphWriter
 {
     public void WriteGraph(string graphData)
     {
-        var graphVizPath = new FileInfo("./input.dot").FullName;
-        File.WriteAllText(graphVizPath, graphData);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var graphVizPath = new FileInfo("./input.dot").FullName;
+            File.WriteAllText(graphVizPath, graphData);
 
-        var fullBatchPath = new FileInfo("./generate-output.bat").FullName;
-        var fullOutputImagePath = new FileInfo("./output.svg").FullName;
-        File.WriteAllText(fullBatchPath, @$"dot -Tsvg input.dot > {fullOutputImagePath}
+            var fullBatchPath = new FileInfo("./generate-output.bat").FullName;
+            var fullOutputImagePath = new FileInfo("./output.svg").FullName;
+            File.WriteAllText(fullBatchPath, @$"dot -Tsvg input.dot > {fullOutputImagePath}
 ""C:\Program Files\Mozilla Firefox\firefox.exe"" {fullOutputImagePath}");
 
-        //System.Diagnostics.Process.Start("explorer.exe", $"/select, \"{fullBatchPath}\"");
-        System.Diagnostics.Process.Start("cmd.exe", $"/c {fullBatchPath}");
+            Process.Start("cmd.exe", $"/c {fullBatchPath}");
+        }
+        
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            var graphVizPath = new FileInfo($"./input.dot").FullName;
+            var fullBatchPath = new FileInfo("./generate-output.sh").FullName;
+            var fullOutputImagePath = new FileInfo($"./output.svg").FullName;
+            
+            File.WriteAllText(graphVizPath, graphData);
+            File.WriteAllText(fullBatchPath, @$"dot -Tsvg {graphVizPath} > {fullOutputImagePath}");
+        }
     }
 }
