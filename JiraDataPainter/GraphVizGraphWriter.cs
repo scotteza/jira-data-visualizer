66348f4f7512cd67a0b015c7cebb9df1ad12e75d
Scott Edwards <scott.edwards@cazoo.co.uk>
2022-11-03 21:07:07 +0000

    Open files after generating on Mac

--- JiraDataPainter/GraphVizGraphWriter.cs
+++ JiraDataPainter/GraphVizGraphWriter.cs
@@ -21,15 +21,53 @@ public class GraphVizGraphWriter : IGraphVizGraphWriter
 
             Process.Start("cmd.exe", $"/c {fullBatchPath}");
         }
-        
+
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            var graphVizPath = new FileInfo($"./input.dot").FullName;
-            var fullBatchPath = new FileInfo("./generate-output.sh").FullName;
-            var fullOutputImagePath = new FileInfo($"./output.svg").FullName;
-            
+            var graphVizPath = new FileInfo($"./input_{dateString}.dot").FullName;
+            var fullBatchPath = new FileInfo($"./generate-output_{dateString}.sh").FullName;
+            var fullOutputImagePath = new FileInfo($"./output_{dateString}.svg").FullName;
+
             File.WriteAllText(graphVizPath, graphData);
-            File.WriteAllText(fullBatchPath, @$"dot -Tsvg {graphVizPath} > {fullOutputImagePath}");
+            File.WriteAllText(fullBatchPath, @$"dot -Tsvg {graphVizPath} > {fullOutputImagePath}{Environment.NewLine}");
+
+            const string command = "sh";
+
+            var processInfo1 = new ProcessStartInfo
+            {
+                FileName = command,
+                Arguments = fullBatchPath,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            var process1 = Process.Start(processInfo1);
+            while (!process1.StandardOutput.EndOfStream)
+            {
+                var result = process1.StandardOutput.ReadLine();
+                Console.WriteLine(result);
+            }
+            
+            process1.WaitForExit();
+
+            var processInfo2 = new ProcessStartInfo
+            {
+                FileName = "open",
+                Arguments = $"{fullOutputImagePath}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            var process2 = Process.Start(processInfo2);
+            while (!process2.StandardOutput.EndOfStream)
+            {
+                var result = process2.StandardOutput.ReadLine();
+                Console.WriteLine(result);
+            }
+
+            process2.WaitForExit();
         }
     }
 }
